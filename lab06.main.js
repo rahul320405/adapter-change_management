d0e3e27d51e818631daad617109ff002b55d521a
rahulshinde31 <rahul.a.shinde@centurylink.com>
2020-05-12 06:59:27 +0000

    Final Project

--- lab06.main.js
+++ lab06.main.js
@@ -192,29 +192,14 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * get() takes a callback function.
      */
-
       this.connector.get((data, error) => {
         if (error) {
-          callback(data, error);
-           console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
-        } else {
-            console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-            if (data.hasOwnProperty('body')) {
-              var body_array = (JSON.parse(data.body));
-              var num_results = body_array.result.length;
-              var changeTicket = [];
-
-              for(var i = 0; i < num_results; i += 1) {
-                var result_array = (JSON.parse(data.body).result);
-                changeTicket.push({"change_ticket_number" : result_array[i].number, "active" : result_array[i].active, "priority" : result_array[i].priority,
-                                   "description" : result_array[i].description, "work_start" : result_array[i].work_start, "work_end" : result_array[i].work_end,
-                                   "change_ticket_key" : result_array[i].sys_id});
-              } 
-              
-              callback(changeTicket, error); 
-            }
-            
-          } 
+          //console.error(`\nError returned from GET request:\n${JSON.stringify(error)}`);
+          return callback(error);
+        }
+        //console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`);
+        
+        return callback(data);
       });
     }    
     
@@ -235,22 +220,13 @@ healthcheck(callback) {
      * Note how the object was instantiated in the constructor().
      * post() takes a callback function.
      */
-      this.connector.post((data, error) => {
+      this.post((data, error) => {
           if (error) {
-            console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
-            callback(data, error);
-          } else {
-              console.log(`\nResponse returned from GET request:\n${JSON.stringify(data)}`)
-            if (data.hasOwnProperty('body')) {
-              var changeTicket = {};
-              var result_array = (JSON.parse(data.body).result);
-              changeTicket = ({"change_ticket_number" : result_array.number, "active" : result_array.active, "priority" : result_array.priority,
-                                   "description" : result_array.description, "work_start" : result_array.work_start, "work_end" : result_array.work_end,
-                                   "change_ticket_key" : result_array.sys_id});
-                console.log('changeTicket get', changeTicket)
-              callback(changeTicket, error); 
-            } 
-           }            
+            // console.error(`\nError returned from POST request:\n${JSON.stringify(error)}`);
+            return callback(error);
+          }
+          // console.log(`\nResponse returned from POST request:\n${JSON.stringify(data)}`);
+          return callback(data);
         });    
     }
 
